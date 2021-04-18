d280115d957cd184a7484660abcc89bd37d9e25e
shunmahi2000 <shunmahiyadav@gmail.com>
2021-04-18 15:34:40 +0530

    first commit

---
+++ ecommerce/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongoose = require("mongoose")
+const morgan = require("morgan")
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const cors = require("cors");
+const expressValidator = require("express-validator")
+// require("dotenv").config();
+const dotenv = require('dotenv');
+dotenv.config()
+//import routes
+const authRoutes = require("./routes/auth")
+const userRoutes = require("./routes/user")
+const categoryRoutes = require("./routes/category")
+const productRoutes = require("./routes/product")
+
+//app
+const app=express();
+
+//db
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+}).then(()=>console.log("DB connected"))
+
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`)
+  });
+
+  
+//middlewares
+app.use(morgan("dev"))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(expressValidator());
+app.use(cors());
+//routes middleware
+app.use("/api",authRoutes);
+app.use("/api",userRoutes);
+app.use("/api",categoryRoutes);
+app.use("/api",productRoutes);
+
+const port = process.env.PORT || 8000
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+});
